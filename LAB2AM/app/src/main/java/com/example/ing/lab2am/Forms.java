d0c1cdd9bb77da9fce66b0be8873e1b476cf269e
ING <ierrazuriz@miuandes.cl>
2018-04-03 12:06:40 -0300

    lab3

---
+++ LAB2AM/app/src/main/java/com/example/ing/lab2am/Forms.java
@@ -0,0 +1,26 @@
+package com.example.ing.lab2am;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import java.text.Normalizer;
+
+@Entity
+public class Forms {
+
+
+        @NonNull
+        @PrimaryKey (autoGenerate = true)
+        private int FormId;
+        private String FormName;
+
+        public Forms() {
+        }
+
+        public int getFormId() { return FormId; }
+        public String getFormName() { return FormName; }
+        public void setFormName (String FormName) { this.FormName = FormName; }
+
+
+}
